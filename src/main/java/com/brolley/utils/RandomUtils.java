a845a5dca521071f5f6d63bf7cb959072c2b6043
kk <kishore.budigapaka@resultifyme.com>
2023-08-14 11:00:57 -0500

    initial commit.

---
+++ src/main/java/com/brolley/utils/RandomUtils.java
@@ -0,0 +1,16 @@
+package com.brolley.utils;
+
+public class RandomUtils {
+
+	public static double getRandom() {
+		System.out.println("inside java method....");
+		return Math.random(); 
+		
+	}
+	public double getRandomIns() {
+		System.out.println("inside java method....");
+		return Math.random(); 
+		
+	}
+
+}
